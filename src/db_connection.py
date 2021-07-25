6714c28578fd831a401bce49afe842530bf1fc81
Gitznik <r.offner@gmx.de>
2021-07-25 19:24:14 +0200

    Add methods to save play history to mongodb

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,6 +1,10 @@
+import pydantic
 from pymongo import MongoClient
 from abc import ABC, abstractmethod
 import pytz
+from typing import List
+
+from .logging.logger import info_logger, debug_logger
 
 class DatabaseConnection(ABC):
 
@@ -12,7 +16,11 @@ class DatabaseConnection(ABC):
         pass
     
     @abstractmethod
-    def save_data(self):
+    def save_one(self):
+        pass
+
+    @abstractmethod
+    def save_many(self):
         pass
 
     @abstractmethod
@@ -29,8 +37,16 @@ class MongoConnection(DatabaseConnection):
             sort=[('played_at', -1)])['played_at']
         return pytz.utc.localize(newest_unaware)
 
-    def save_data(self, data: dict) -> None:
-        self.conn.insert_one(data)
+    def save_one(self, data: pydantic.BaseModel) -> None:
+        inserted = self.conn.insert_one(data.dict())
+        debug_logger.debug(f'Inserted {inserted.inserted_id}')
+
+
+    def save_many(self, data: List[pydantic.BaseModel]) -> None:
+        data_parsed = [item.dict() for item in data]
+        inserted = self.conn.insert_many(data_parsed)
+        debug_logger.debug(f'Inserted {inserted.inserted_ids}')
+        
 
     def reset_collection(self) -> None:
         self.conn.delete_many({})
