4fcf850f2970f4111d044f5a4e39a588d41603df
Gitznik <r.offner@gmx.de>
2021-08-01 13:13:05 +0200

    Add error handling for malformed body in spotify response

--- src/errors/http_errors.py
+++ src/errors/http_errors.py
@@ -1,8 +1,16 @@
+from json.decoder import JSONDecodeError
+from logging import info
 from requests.models import HTTPError
+from ..logging.logger import info_logger
 
 class SpotifyHttpError(Exception):
     def __init__(self, e: HTTPError, msg: str = ''):
         original_error = str(e)
-        spotify_error = e.response.json()['error']['message']
+        try:
+            spotify_error = e.response.json()['error']['message']
+        except JSONDecodeError as err:
+            info_logger.warning(
+                'Unable to parse spotify error response body' , exc_info=True)
+            spotify_error = e.response.text
         self.msg = f'{original_error} --- {spotify_error} --- {msg}'
         super().__init__(self.msg)
